d6528470b7beb80e4844e0a1cd07b09b853616db
anarbekovv11 <anarbekovvv11@gamil.com>
2023-02-25 12:15:03 +0600

    home work4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Form extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {value: ""}
+		
+		this.handleChange = this.handleChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+	handleChange(e){
+		this.setState({value: e.target.value})
+	}
+	handleSubmit(e){
+		alert("Email address : " + this.state.value).input.value = ""
+		e.preventDefault();
+	}
+	render(){
+		return (
+			<div className="menu">
+        <div className="main-block">
+        <h1>Авторизация</h1>
+				<form onSubmit={this.handleSubmit}>
+					<div className="inputBox">
+						<input type="text" required="required" className="input" value={this.state.value} onChange={this.handleChange}></input>
+						<span class="span">Email</span>
+					</div>
+					<div className="inputBox">
+						<input type="password" required="required" className="input"/>
+						<span class ="span">Password</span>
+					</div>
+					<div className="btn">
+						<input type="submit" value="Регистрация" className="submit"/>
+					</div>
+				</form>
+        </div>
+			</div>	
+		)
+	}
 }
 
-export default App;
+
+export default Form;
